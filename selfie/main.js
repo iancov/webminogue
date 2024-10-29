5f84526e1b39904ee708911ed71d1fe8eb86404f
iancov <idcovert23@gmail.com>
2024-10-29 01:40:41 -0400

    adding selfie!

---
+++ selfie/main.js
@@ -0,0 +1,413 @@
+function leftChange() {
+    console.log(document.getElementById("leftImage").src);
+    if (document.getElementById("leftImage").getAttribute("src") == "left01.png")
+    {
+        document.getElementById("leftImage").src = "left02.png";
+    }
+
+    else if (document.getElementById("leftImage").getAttribute("src") == "left02.png")
+    {
+        document.getElementById("leftImage").src = "left03.png";
+    }
+
+    else if (document.getElementById("leftImage").getAttribute("src") == "left03.png")
+        {
+            document.getElementById("leftImage").src = "left04.png";
+        }
+
+    else if (document.getElementById("leftImage").getAttribute("src") == "left04.png")
+        {
+            document.getElementById("leftImage").src = "left05.png";
+        }
+
+    else if (document.getElementById("leftImage").getAttribute("src") == "left05.png")
+        {
+            document.getElementById("leftImage").src = "left06.png";
+        }
+        
+    else if (document.getElementById("leftImage").getAttribute("src") == "left06.png")
+        {
+            document.getElementById("leftImage").src = "left07.png";
+        }
+
+    else if (document.getElementById("leftImage").getAttribute("src") == "left07.png")
+        {
+            document.getElementById("leftImage").src = "left08.png";
+        }
+
+    else if (document.getElementById("leftImage").getAttribute("src") == "left08.png")
+        {
+            document.getElementById("leftImage").src = "left09.png";
+        }
+        
+    else if (document.getElementById("leftImage").getAttribute("src") == "left09.png")
+        {
+            document.getElementById("leftImage").src = "left10.png";
+        }
+
+    else if (document.getElementById("leftImage").getAttribute("src") == "left10.png")
+        {
+            document.getElementById("leftImage").src = "left11.png";
+        }
+
+    else if (document.getElementById("leftImage").getAttribute("src") == "left11.png")
+        {
+            document.getElementById("leftImage").src = "left12.png";
+        }
+        
+    else if (document.getElementById("leftImage").getAttribute("src") == "left12.png")
+        {
+            document.getElementById("leftImage").src = "left13.png";
+        }
+
+    else if (document.getElementById("leftImage").getAttribute("src") == "left13.png")
+        {
+            document.getElementById("leftImage").src = "left14.png";
+        }
+
+    else if (document.getElementById("leftImage").getAttribute("src") == "left14.png")
+        {
+            document.getElementById("leftImage").src = "left15.png";
+        }
+
+    else if (document.getElementById("leftImage").getAttribute("src") == "left15.png")
+        {
+            document.getElementById("leftImage").src = "left16.png";
+        }
+
+    else if (document.getElementById("leftImage").getAttribute("src") == "left16.png")
+        {
+            document.getElementById("leftImage").src = "left01.png";
+        }
+}
+
+function rightChange() {
+    console.log(document.getElementById("rightImage").src);
+    if (document.getElementById("rightImage").getAttribute("src") == "right01.png")
+    {
+        document.getElementById("rightImage").src = "right02.png";
+    }
+
+    else if (document.getElementById("rightImage").getAttribute("src") == "right02.png")
+    {
+        document.getElementById("rightImage").src = "right03.png";
+    }
+
+    else if (document.getElementById("rightImage").getAttribute("src") == "right03.png")
+        {
+            document.getElementById("rightImage").src = "right04.png";
+        }
+
+    else if (document.getElementById("rightImage").getAttribute("src") == "right04.png")
+        {
+            document.getElementById("rightImage").src = "right05.png";
+        }
+    
+    else if (document.getElementById("rightImage").getAttribute("src") == "right05.png")
+        {
+            document.getElementById("rightImage").src = "right06.png";
+        }
+
+    else if (document.getElementById("rightImage").getAttribute("src") == "right06.png")
+        {
+            document.getElementById("rightImage").src = "right07.png";
+        }
+    
+    else if (document.getElementById("rightImage").getAttribute("src") == "right07.png")
+        {
+            document.getElementById("rightImage").src = "right08.png";
+        }
+
+    else if (document.getElementById("rightImage").getAttribute("src") == "right08.png")
+        {
+            document.getElementById("rightImage").src = "right09.png";
+        }
+    
+    else if (document.getElementById("rightImage").getAttribute("src") == "right09.png")
+        {
+            document.getElementById("rightImage").src = "right10.png";
+        }
+
+    else if (document.getElementById("rightImage").getAttribute("src") == "right10.png")
+        {
+            document.getElementById("rightImage").src = "right11.png";
+        }
+    
+    else if (document.getElementById("rightImage").getAttribute("src") == "right11.png")
+        {
+            document.getElementById("rightImage").src = "right12.png";
+        }
+
+    else if (document.getElementById("rightImage").getAttribute("src") == "right12.png")
+        {
+            document.getElementById("rightImage").src = "right13.png";
+        }
+    
+    else if (document.getElementById("rightImage").getAttribute("src") == "right13.png")
+        {
+            document.getElementById("rightImage").src = "right14.png";
+        }
+
+    else if (document.getElementById("rightImage").getAttribute("src") == "right14.png")
+        {
+            document.getElementById("rightImage").src = "right15.png";
+        }
+    
+    else if (document.getElementById("rightImage").getAttribute("src") == "right15.png")
+        {
+            document.getElementById("rightImage").src = "right16.png";
+        }
+
+    else if (document.getElementById("rightImage").getAttribute("src") == "right16.png")
+        {
+            document.getElementById("rightImage").src = "right01.png";
+        }
+}
+
+function noseChange() {
+    console.log(document.getElementById("noseImage").src);
+    if (document.getElementById("noseImage").getAttribute("src") == "nose01.png")
+    {
+        document.getElementById("noseImage").src = "nose02.png";
+    }
+
+    else if (document.getElementById("noseImage").getAttribute("src") == "nose02.png")
+    {
+        document.getElementById("noseImage").src = "nose03.png";
+    }
+
+    else if (document.getElementById("noseImage").getAttribute("src") == "nose03.png")
+        {
+            document.getElementById("noseImage").src = "nose04.png";
+        }
+
+    else if (document.getElementById("noseImage").getAttribute("src") == "nose04.png")
+        {
+            document.getElementById("noseImage").src = "nose05.png";
+        }
+    
+    else if (document.getElementById("noseImage").getAttribute("src") == "nose05.png")
+        {
+            document.getElementById("noseImage").src = "nose06.png";
+        }
+
+    else if (document.getElementById("noseImage").getAttribute("src") == "nose06.png")
+        {
+            document.getElementById("noseImage").src = "nose07.png";
+        }
+    
+    else if (document.getElementById("noseImage").getAttribute("src") == "nose07.png")
+        {
+            document.getElementById("noseImage").src = "nose08.png";
+        }
+
+    else if (document.getElementById("noseImage").getAttribute("src") == "nose08.png")
+        {
+            document.getElementById("noseImage").src = "nose09.png";
+        }
+    
+    else if (document.getElementById("noseImage").getAttribute("src") == "nose09.png")
+        {
+            document.getElementById("noseImage").src = "nose10.png";
+        }
+
+    else if (document.getElementById("noseImage").getAttribute("src") == "nose10.png")
+        {
+            document.getElementById("noseImage").src = "nose11.png";
+        }
+    
+    else if (document.getElementById("noseImage").getAttribute("src") == "nose11.png")
+        {
+            document.getElementById("noseImage").src = "nose12.png";
+        }
+
+    else if (document.getElementById("noseImage").getAttribute("src") == "nose12.png")
+        {
+            document.getElementById("noseImage").src = "nose13.png";
+        }
+    
+    else if (document.getElementById("noseImage").getAttribute("src") == "nose13.png")
+        {
+            document.getElementById("noseImage").src = "nose14.png";
+        }
+
+    else if (document.getElementById("noseImage").getAttribute("src") == "nose14.png")
+        {
+            document.getElementById("noseImage").src = "nose15.png";
+        }
+    
+    else if (document.getElementById("noseImage").getAttribute("src") == "nose15.png")
+        {
+            document.getElementById("noseImage").src = "nose16.png";
+        }
+
+    else if (document.getElementById("noseImage").getAttribute("src") == "nose16.png")
+        {
+            document.getElementById("noseImage").src = "nose01.png";
+        }
+}
+
+function mouthChange() {
+    console.log(document.getElementById("mouthImage").src);
+    if (document.getElementById("mouthImage").getAttribute("src") == "mouth01.png")
+    {
+        document.getElementById("mouthImage").src = "mouth02.png";
+    }
+
+    else if (document.getElementById("mouthImage").getAttribute("src") == "mouth02.png")
+    {
+        document.getElementById("mouthImage").src = "mouth03.png";
+    }
+
+    else if (document.getElementById("mouthImage").getAttribute("src") == "mouth03.png")
+        {
+            document.getElementById("mouthImage").src = "mouth04.png";
+        }
+        
+    else if (document.getElementById("mouthImage").getAttribute("src") == "mouth04.png")
+        {
+            document.getElementById("mouthImage").src = "mouth05.png";
+        }
+
+    else if (document.getElementById("mouthImage").getAttribute("src") == "mouth05.png")
+        {
+            document.getElementById("mouthImage").src = "mouth06.png";
+        }
+    
+    else if (document.getElementById("mouthImage").getAttribute("src") == "mouth06.png")
+        {
+            document.getElementById("mouthImage").src = "mouth07.png";
+        }
+        
+    else if (document.getElementById("mouthImage").getAttribute("src") == "mouth07.png")
+        {
+            document.getElementById("mouthImage").src = "mouth08.png";
+        }
+
+    else if (document.getElementById("mouthImage").getAttribute("src") == "mouth08.png")
+        {
+            document.getElementById("mouthImage").src = "mouth09.png";
+        }
+    
+    else if (document.getElementById("mouthImage").getAttribute("src") == "mouth09.png")
+        {
+            document.getElementById("mouthImage").src = "mouth10.png";
+        }
+        
+    else if (document.getElementById("mouthImage").getAttribute("src") == "mouth10.png")
+        {
+            document.getElementById("mouthImage").src = "mouth11.png";
+        }
+
+    else if (document.getElementById("mouthImage").getAttribute("src") == "mouth11.png")
+        {
+            document.getElementById("mouthImage").src = "mouth12.png";
+        }
+    
+    else if (document.getElementById("mouthImage").getAttribute("src") == "mouth12.png")
+        {
+            document.getElementById("mouthImage").src = "mouth13.png";
+        }
+        
+    else if (document.getElementById("mouthImage").getAttribute("src") == "mouth13.png")
+        {
+            document.getElementById("mouthImage").src = "mouth14.png";
+        }
+
+    else if (document.getElementById("mouthImage").getAttribute("src") == "mouth14.png")
+        {
+            document.getElementById("mouthImage").src = "mouth15.png";
+        }
+
+    else if (document.getElementById("mouthImage").getAttribute("src") == "mouth15.png")
+        {
+            document.getElementById("mouthImage").src = "mouth16.png";
+        }
+    
+    else if (document.getElementById("mouthImage").getAttribute("src") == "mouth16.png")
+        {
+            document.getElementById("mouthImage").src = "mouth01.png";
+        }
+
+}
+
+function backChange() {
+    console.log(document.getElementById("backImage").src);
+    if (document.getElementById("backImage").getAttribute("src") == "back01.png")
+    {
+        document.getElementById("backImage").src = "back02.png";
+    }
+
+    else if (document.getElementById("backImage").getAttribute("src") == "back02.png")
+    {
+        document.getElementById("backImage").src = "back03.png";
+    }
+
+    else if (document.getElementById("backImage").getAttribute("src") == "back03.png")
+    {
+        document.getElementById("backImage").src = "back04.png";
+    }
+    
+    else if (document.getElementById("backImage").getAttribute("src") == "back04.png")
+    {
+        document.getElementById("backImage").src = "back05.png";
+    }
+        
+    else if (document.getElementById("backImage").getAttribute("src") == "back05.png")
+    {
+        document.getElementById("backImage").src = "back06.png";
+    }
+    else if (document.getElementById("backImage").getAttribute("src") == "back06.png")
+    {
+        document.getElementById("backImage").src = "back07.png";
+    }
+    
+    else if (document.getElementById("backImage").getAttribute("src") == "back07.png")
+    {
+        document.getElementById("backImage").src = "back08.png";
+    }
+    
+    else if (document.getElementById("backImage").getAttribute("src") == "back08.png")
+    {
+        document.getElementById("backImage").src = "back09.png";
+    }
+        
+    else if (document.getElementById("backImage").getAttribute("src") == "back09.png")
+    {
+        document.getElementById("backImage").src = "back10.png";
+    }
+
+    else if (document.getElementById("backImage").getAttribute("src") == "back10.png")
+    {
+        document.getElementById("backImage").src = "back11.png";
+    }
+
+    else if (document.getElementById("backImage").getAttribute("src") == "back11.png")
+    {
+        document.getElementById("backImage").src = "back12.png";
+    }
+    
+    else if (document.getElementById("backImage").getAttribute("src") == "back12.png")
+    {
+        document.getElementById("backImage").src = "back13.png";
+    }
+        
+    else if (document.getElementById("backImage").getAttribute("src") == "back13.png")
+    {
+        document.getElementById("backImage").src = "back14.png";
+    }
+    else if (document.getElementById("backImage").getAttribute("src") == "back14.png")
+    {
+        document.getElementById("backImage").src = "back15.png";
+    }
+    
+    else if (document.getElementById("backImage").getAttribute("src") == "back15.png")
+    {
+        document.getElementById("backImage").src = "back16.png";
+    }
+    
+    else if (document.getElementById("backImage").getAttribute("src") == "back16.png")
+    {
+        document.getElementById("backImage").src = "back01.png";
+    }
+}
